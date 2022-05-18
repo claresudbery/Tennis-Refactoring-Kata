4bd43210e7e36cd660aa2d094d0b510c223cc992
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-05-19 00:17:22 +0100

    Rename vars and inline var

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,41 +2,40 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int _player2Points;
+        private int _player1Points;
+        private string _player1Name;
+        private string _player2Name;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this._player1Name = player1Name;
+            this._player2Name = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            if ((_player1Points < 4 && _player2Points < 4) && (_player1Points + _player2Points < 6))
             {
                 string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                return (_player1Points == _player2Points) ? p[_player1Points] + "-All" : p[_player1Points] + "-" + p[_player2Points];
             }
             else
             {
-                if (p1 == p2)
+                string s;
+                if (_player1Points == _player2Points)
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                s = _player1Points > _player2Points ? _player1Name : _player2Name;
+                return ((_player1Points - _player2Points) * (_player1Points - _player2Points) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                this._player1Points += 1;
             else
-                this.p2 += 1;
+                this._player2Points += 1;
         }
 
     }
