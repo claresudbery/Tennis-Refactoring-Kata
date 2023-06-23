10572a9fd1fa0c683deda6c89200a6cb1597a14b
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-23 15:07:00 +0100

    renamed player vars

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p1;
-        private int p2;
+        private int player1;
+        private int player2;
         
         private string p1N;
         private string p2N;
@@ -17,27 +17,27 @@ namespace Tennis
         public string GetScore()
         {
             string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            if ((player1 < 4 && player2 < 4) && (player1 + player2 < 6))
             {
                 string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                s = p[player1];
+                return (player1 == player2) ? s + "-All" : s + "-" + p[player2];
             }
             else
             {
-                if (p1 == p2)
+                if (player1 == player2)
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                s = player1 > player2 ? p1N : p2N;
+                return ((player1 - player2) * (player1 - player2) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                this.player1 += 1;
             else
-                this.p2 += 1;
+                this.player2 += 1;
         }
 
     }
